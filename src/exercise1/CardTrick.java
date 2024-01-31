a27b8c354bd0331f576e207d94d015fa394aef21
Harsimran Singh <sin14134@sheridancollege.ca>
2024-01-31 07:35:04 -0800

    2 commit

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,27 +1,58 @@
 package exercise1;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
  * It then searches the array of cards for the match to the user's card. 
  * To be used as starting code in Exercise
  *
- * @author dancye
- * @author Paul Bonenfant Jan 25, 2022 
+ * @author Harsimran Singh
+ * @author Paul Bonenfant Jan 31, 2024 
  */
 public class CardTrick {
-    
+
     public static void main(String[] args) {
-        
+        Random random = new Random();
+        int compGuess = random.nextInt(13) + 1; // Make sure to include 13 in the random range
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your guess (1-13): ");
+        int userInput = input.nextInt();
+
         Card[] hand = new Card[7];
+        hand[0] = new Card("Diamonds", 1);
+        hand[1] = new Card("Spades", 11);
+        hand[2] = new Card("Clubs", 12);
+        hand[3] = new Card("Hearts", 13);
+        hand[4] = new Card("Diamonds", 10);
+        hand[5] = new Card("Spades", 9);
+        hand[6] = new Card("Clubs", 7);
+
+        boolean found = false;
 
         for (int i = 0; i < hand.length; i++) {
-            Card card = new Card();
+            if (userInput == hand[i].getValue()) {
+                found = true;
+                System.out.println("Your guess is right!");
+                printInfo();
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Wrong guess. No match found in the hand.");
+        }
+    
+    
+            
             //card.setValue(insert call to random number generator here)
             // 
             //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
             // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
             //       Don't worry about duplicates at this point
-        }
+        
 
         // insert code to ask the user for Card value and suit, create their card
         // and search the hand here. 
@@ -62,6 +93,6 @@ public class CardTrick {
         System.out.println();
         
     
-    }
+}
 
 }

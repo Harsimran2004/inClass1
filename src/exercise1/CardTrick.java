33771795de83455ddbf86fee58792fe0a63221fa
Harsimran Singh <sin14134@sheridancollege.ca>
2024-01-31 07:40:05 -0800

    added my info

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -72,11 +72,12 @@ public class CardTrick {
      * @author Paul Bonenfant Jan 2022
      */
     private static void printInfo() {
+        // I am done
     
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Harsimran Singh, but you can call me prof, Hari or sir");
         System.out.println();
         
         System.out.println("My career ambitions:");
@@ -85,7 +86,7 @@ public class CardTrick {
 	System.out.println();	
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
+        System.out.println("-- coding");
         System.out.println("-- Cooking");
         System.out.println("-- Reading/Watching TV");
         System.out.println("-- Riding my motorcycle");
